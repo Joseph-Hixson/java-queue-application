3a81df0c6693d6979acd06ee16989c252fc6276e
Joseph-Hixson <josephdhf@hotmail.com>
2025-02-27 13:55:22 -0600

    added spaces to toString method message.

--- javaQueueApplicationJosephHixson/src/model/Task.java
+++ javaQueueApplicationJosephHixson/src/model/Task.java
@@ -25,6 +25,6 @@ public class Task {
 	
 	@Override
 	public String toString() {
-		return "Task [id=" + id + ", description=" + description + "]";
+		return "Task [id = " + id + ", description = " + description + "]";
 	}
 }

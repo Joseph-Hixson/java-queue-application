8de129a54b0b9b5f398ed4893d900df1af878cf6
Joseph-Hixson <josephdhf@hotmail.com>
2025-02-27 11:26:13 -0600

    Created switch cases to add task and case to print task list.

--- javaQueueApplicationJosephHixson/src/app/QueueApp.java
+++ javaQueueApplicationJosephHixson/src/app/QueueApp.java
@@ -1,9 +1,49 @@
 package app;
 
+import service.TaskQueueService;
+import java.util.Scanner;
+
+import model.Task;
 public class QueueApp {
 
 	public static void main(String[] args) {
+		TaskQueueService queueService = new TaskQueueService();
+		Scanner scanner = new Scanner(System.in);
+		int input = 0;
+		while (input < 4) {
+			System.out.println("Press 1 to Add a Task to the Queue: ");
+			System.out.println("Press 2 to List All the Tasks: ");
+			System.out.println("Press 3 to Remove a Task: ");
+			System.out.println("Press 4 to Exit the Application: ");
+			input = scanner.nextInt();
+			scanner.nextLine();
 		
-		
+			switch(input) {
+				case 1:
+					
+					System.out.println("Enter the Task Id: ");
+					int id = scanner.nextInt();
+					scanner.nextLine();
+					System.out.println("Enter the Task Description: ");
+					String description = scanner.nextLine();
+					Task task = new Task(id, description);
+					queueService.enqueue(task);
+					System.out.println("Task Added.");
+					break;
+				case 2:
+					for(Task t : queueService.listTasks()) {
+						System.out.println(t);
+					}
+					break;
+				case 3:
+				System.out.println("");
+				
+				case 4:
+				System.out.println("System exiting...");
+				break;
+			}
+		}
+		scanner.close();
 	}
+	
 }
